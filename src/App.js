1b071597643663fab1ab3ff1adbeb1be3fe577e9
Sohaib <sohaibbutt448506@gmail.com>
2022-07-01 00:18:30 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,188 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
+import mainImage from "./asset/diagram-01.png";
+import img1 from "./asset/diagram-02.png";
+import img2 from "./asset/diagram-03.png";
+import img3 from "./asset/diagram-04.png";
+import img4 from "./asset/diagram-05.png";
+import img5 from "./asset/diagram-06.png";
+import img6 from "./asset/diagram-07.png";
 function App() {
+  const [isHovering, setIsHovering] = useState(false);
+  const handleMouseOver = () => {
+    setIsHovering(true);
+  };
+  const handleMouseOut = () => {
+    setIsHovering(false);
+  };
+  //img2
+  const [isHovering2, setIsHovering2] = useState(false);
+  const handleMouseOver2 = () => {
+    setIsHovering2(true);
+  };
+  const handleMouseOut2 = () => {
+    setIsHovering2(false);
+  };
+
+  //img3
+  const [isHovering3, setIsHovering3] = useState(false);
+  const handleMouseOver3 = () => {
+    setIsHovering3(true);
+  };
+  const handleMouseOut3 = () => {
+    setIsHovering3(false);
+  };
+  //img4
+  const [isHovering4, setIsHovering4] = useState(false);
+  const handleMouseOver4 = () => {
+    setIsHovering4(true);
+  };
+  const handleMouseOut4 = () => {
+    setIsHovering4(false);
+  };
+  //img5
+  const [isHovering5, setIsHovering5] = useState(false);
+  const handleMouseOver5 = () => {
+    setIsHovering5(true);
+  };
+  const handleMouseOut5 = () => {
+    setIsHovering5(false);
+  };
+  //img6
+  const [isHovering6, setIsHovering6] = useState(false);
+  const handleMouseOver6 = () => {
+    setIsHovering6(true);
+  };
+  const handleMouseOut6 = () => {
+    setIsHovering6(false);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="container container-logo">
+        <div className="main-image">
+          <div className="forWidth">
+            <a href="">
+              {" "}
+              <img src={mainImage} alt="" />
+            </a>
+          </div>
+        </div>
+        <div
+          className="img1"
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <a href="">
+            {" "}
+            <img src={img1} alt="" />
+          </a>
+        </div>
+        <div
+          className="img2"
+          onMouseOver={handleMouseOver2}
+          onMouseOut={handleMouseOut2}
+        >
+          <a href="">
+            {" "}
+            <img src={img4} alt="" />
+          </a>
+        </div>
+        <div
+          className="img3"
+          onMouseOver={handleMouseOver3}
+          onMouseOut={handleMouseOut3}
+        >
+          <a href="">
+            {" "}
+            <img src={img2} alt="" />
+          </a>
+        </div>
+        <div
+          className="img4"
+          onMouseOver={handleMouseOver4}
+          onMouseOut={handleMouseOut4}
+        >
+          <a href="">
+            {" "}
+            <img src={img3} alt="" />
+          </a>
+        </div>
+        <div
+          className="img5"
+          onMouseOver={handleMouseOver5}
+          onMouseOut={handleMouseOut5}
+        >
+          <a href="">
+            {" "}
+            <img src={img6} alt="" />
+          </a>
+        </div>
+        <div
+          className="img6"
+          onMouseOver={handleMouseOver6}
+          onMouseOut={handleMouseOut6}
+        >
+          <a href="">
+            {" "}
+            <img src={img5} alt="" />
+          </a>
+        </div>
+        <div className={isHovering ? "description active" : "description"}>
+          <div className="hexagon p1">
+            <p>
+              {" "}
+              Img 1Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Nihil dolores qui commodi inventore? Deserunt aut nesciunt eaq
+            </p>
+          </div>
+        </div>
+        <div className={isHovering2 ? "description active" : "description"}>
+          <div className="hexagon p2">
+            <p>
+              {" "}
+              Img2 Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Nihil dolores qui commodi inventore? Deserunt aut nesciunt e
+            </p>
+          </div>
+        </div>
+        <div className={isHovering3 ? "description active" : "description"}>
+          <div className="hexagon p2">
+            <p>
+              {" "}
+              Img3 Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Nihil dolores qui commodi inventore? Deserunt aut nesciunt e
+            </p>
+          </div>
+        </div>
+        <div className={isHovering4 ? "description active" : "description"}>
+          <div className="hexagon p2">
+            <p>
+              {" "}
+              Img4 Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Nihil dolores qui commodi inventore? Deserunt aut nesciunt e
+            </p>
+          </div>
+        </div>
+        <div className={isHovering5 ? "description active" : "description"}>
+          <div className="hexagon p2">
+            <p>
+              {" "}
+              Img5 Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Nihil dolores qui commodi inventore? Deserunt aut nesciunt e
+            </p>
+          </div>
+        </div>
+        <div className={isHovering6 ? "description active" : "description"}>
+          <div className="hexagon p2">
+            <p>
+              {" "}
+              Img6 Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Nihil dolores qui commodi inventore? Deserunt aut nesciunt e
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
